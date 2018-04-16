37b69e765305e989bb79e0492788f4dc3c2c7c2b
Derek Johnson <drk.w.jhnsn@gmail.com>
2018-04-15 18:02:50 -0700

    Remove unnecessary SearchBarContainer component

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import SearchBarContainer from './SearchBarContainer.jsx';
+// import SearchBarContainer from './SearchBarContainer.jsx';
+import axios from 'axios';
+import SearchBar from '../components/SearchBar.jsx';
 import Gallery from '../components/Gallery.jsx';
 import ImageModal from '../components/ImageModal.jsx';
 
@@ -7,17 +9,30 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      userPhrase: '',
       images: [],
       modalImageIdx: -1
     }
 
-    this.receiveImageMeta = this.receiveImageMeta.bind(this);
+    this.fetchResults = this.fetchResults.bind(this);
     this.selectModalImage = this.selectModalImage.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
-  receiveImageMeta(images) {
-    this.setState({ images });
+  fetchResults(searchPhrase) {
+    if (searchPhrase === '') return;
+    this.setState({userPhrase: searchPhrase});
+    axios.post('/search', { searchPhrase })
+    .then((response) => {
+      var { images, result_count } = response.data;
+      this.setState({
+        images,
+        resultCount: result_count
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
 
   selectModalImage(modalImageIdx) {
@@ -38,7 +53,7 @@ export default class App extends Component {
 
     return (
       <div>
-        <SearchBarContainer passResults={this.receiveImageMeta} />
+        <SearchBar submitPhrase={this.fetchResults}/>
         <Gallery images={images} handleSelection={this.selectModalImage} />
         { ternaryWrappedModal }
       </div>
